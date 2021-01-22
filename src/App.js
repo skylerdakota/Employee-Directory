d164b2f74bced130384bb848a4970fe892436a24
Skyler Dakota Lawton <skylerdakota@gmail.com>
2021-01-22 13:39:06 -0500

    changing from ombdcontainer to pages method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
 import React from "react";
-import OmdbContainer from "./components/OmdbContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import employees from "./pages/employees";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
 function App() {
-  return <OmdbContainer />;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={employees} />
+          <Route exact path="/employees" component={employees} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
 }
+
 
 export default App;
